837128e8f15abc1af5223d7a0ee83bd67b365e4f
Rayssa Guimarães <guimaraesr305@gmail.com>
2025-01-14 12:20:31 -0300

    add project reference and new migration

--- OrderAPI/Migrations/20250114150008_ChangingProductIdToProductName.Designer.cs
+++ OrderAPI/Migrations/20250114150008_ChangingProductIdToProductName.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using OrderAPI.Data;
@@ -11,9 +12,11 @@ using OrderAPI.Data;
 namespace OrderAPI.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250114150008_ChangingProductIdToProductName")]
+    partial class ChangingProductIdToProductName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OrderAPI/Migrations/20250114150008_ChangingProductIdToProductName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangingProductIdToProductName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductId",
+                table: "OrderItems",
+                newName: "ProductName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductName",
+                table: "OrderItems",
+                newName: "ProductId");
+        }
+    }
+}
