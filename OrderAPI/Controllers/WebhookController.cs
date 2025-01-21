8eb28c149beecb9e51a32535ed7997d83a995774
Rayssa Guimarães <guimaraesr305@gmail.com>
2025-01-21 07:14:33 -0300

    add stripe endpoint

--- OrderAPI/Controllers/WebhookController.cs
+++ OrderAPI/Controllers/WebhookController.cs
@@ -1,24 +1,67 @@
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
+using OrderAPI.Interfaces;
 using Stripe;
-
+using Stripe.Checkout;
 
 namespace OrderAPI.Controllers
 {
     [Route("webhook")]
     [ApiController]
-    public class WebhookController
+    public class WebhookController : ControllerBase
     {
         private readonly IConfiguration _config;
-        public WebhookController(IConfiguration config)
+        private readonly IPaymentService _paymentsService;
+        public WebhookController(IConfiguration config, IPaymentService paymentsService)
         {
             _config = config;
-            StripeConfiguration.ApiKey = _config["Stripe:ApiKey"];
+            _paymentsService = paymentsService;
+
         }
 
-        
-        // [HttpPost]
-        // public async Task<IActionResult> TestStripeWebhook([FromBody]){
+        [HttpGet]
+        public async Task<IActionResult> HandleSuccessRedirect([FromQuery] string session_id)
+        {
+            if (string.IsNullOrEmpty(session_id))
+            {
+                return BadRequest("Session ID is required.");
+            }
 
-        // }
+            try
+            {
+                StripeConfiguration.ApiKey = _config["Stripe:ApiKey"];
+                var service = new SessionService();
+
+                var session = await service.GetAsync(session_id);
+
+                if (session != null && session.PaymentStatus == "paid")
+                {
+                    var paymentIntentService = new PaymentIntentService();
+                    var paymentIntent = paymentIntentService.Get(session.PaymentIntentId);
+
+                    var paymentDto = await _paymentsService.GetPaymentSuccessData(session.Id);
+
+                    if (paymentDto != null)
+                    {
+                        Console.WriteLine(paymentDto.User.ToString());
+                        Console.WriteLine(paymentDto.Order.ToString());
+                        await _paymentsService.onPaymentSuccess(paymentDto.User, paymentDto.Order);
+                        Console.WriteLine($"Pagamento bem-sucedido para a sessão: {session.Id}");
+                        return Ok("Pagamento processado com sucesso!");
+                    }
+                    else
+                    {
+                        return BadRequest("Erro ao processar o pagamento.");
+                    }
+                }
+
+                return BadRequest("Sessão de pagamento não foi concluída com sucesso.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao processar o pagamento: {ex.Message}");
+                return StatusCode(500, "Erro interno ao processar o pagamento.");
+            }
+        }
     }
 }
