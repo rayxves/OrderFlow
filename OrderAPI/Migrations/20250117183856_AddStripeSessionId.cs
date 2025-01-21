8bbfa1f387025704e0eda76fc2f9023fe1f80584
Rayssa Guimarães <guimaraesr305@gmail.com>
2025-01-21 07:11:50 -0300

    add migrations to use docker images

---
+++ OrderAPI/Migrations/20250117183856_AddStripeSessionId.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStripeSessionId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StripePaymentIntentId",
+                table: "Payments",
+                newName: "StripeSessionId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentUrl",
+                table: "Payments",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentUrl",
+                table: "Payments");
+
+            migrationBuilder.RenameColumn(
+                name: "StripeSessionId",
+                table: "Payments",
+                newName: "StripePaymentIntentId");
+        }
+    }
+}
